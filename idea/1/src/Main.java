2806782a3a21e8fbcdad4f6767ee4d90b78d6e8f
xcl0608 <2385590661@qq.com>
2024-09-14 22:03:22 +0800

    1
    1

---
+++ idea/1/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Mytool mt = new Mytool();
+        int []arr1 = {1,3,2,5,4};
+         mt.bubble(arr1);
+        for (int j : arr1) {
+            System.out.println(j);
+            
+        }
+    }
+}
+class Mytool{
+    public void bubble(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr.length-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j+1];
+                    arr[j+1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
